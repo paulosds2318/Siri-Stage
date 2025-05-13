bc1ee3987f25c5b8553f334490f5d0ee23bc69d8
Eduardo Henrique <dudu30rafa@gmail.com>
2025-05-13 00:11:50 -0300

    Create crud_empresas.py

--- src/crud_empresas.py
+++ src/crud_empresas.py
@@ -1,1 +1,5 @@
+<<<<<<< Updated upstream
 # Pedro Vinicius - CRUD de Empresas (perfil da Empresa: criar/editar/excluir/ver)
+=======
+#CRUD das candidaturas (Pedro Vinicius): perfil das Empresas.
+>>>>>>> Stashed changes
