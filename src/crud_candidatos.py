c402b3591fd6411e566a1024f4b6ac7e4ce11aa4
Luiz Henrique <luiz.contato.1000@gmail.com>
2025-05-12 23:43:51 -0300

    Criei a função criar usuarios

--- src/crud_candidatos.py
+++ src/crud_candidatos.py
@@ -1,1 +1,9 @@
 #CRUD do perfil dos candidatos (Luís Henrique).
+lista_usuarios = []
+def criar_usuario():
+    usuario = {}
+    usuario["Nome"] = str(input("Nome do usuário: "))
+    usuario["Idade"] = int(input("Digite sua idade: "))
+    usuario["Sexo"] = str(input("Qual seu sexo? "))
+    lista_usuarios.append(usuario.copy())
+    print(lista_usuarios)
