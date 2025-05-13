aa1ff6ecfac6ba411a0cc426dbbad0fea7c33dd3
Luiz Henrique <luiz.contato.1000@gmail.com>
2025-05-13 00:13:56 -0300

    adicionando menu inicial

--- src/crud_candidatos.py
+++ src/crud_candidatos.py
@@ -1,9 +1,20 @@
 #CRUD do perfil dos candidatos (Luís Henrique).
 lista_usuarios = []
+
+def menu_inical():
+    print("1 - Criar Usuário")
+    print("2 - Listar Usuário")
+    print("3 - Editar Usuário")
+    print("4 - Deletar Usuário")
+
+
 def criar_usuario():
     usuario = {}
     usuario["Nome"] = str(input("Nome do usuário: "))
     usuario["Idade"] = int(input("Digite sua idade: "))
     usuario["Sexo"] = str(input("Qual seu sexo? "))
     lista_usuarios.append(usuario.copy())
+    print("-"*50)
+    print('Usuário Cadastrado Com Sucesso! ')
+    print("-"*50)
     print(lista_usuarios)
