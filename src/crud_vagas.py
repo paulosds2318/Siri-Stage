685d1503f74f82d40f2342833020f92d6a7a794a
Paulo Marrocos <pcsm@cesar.school>
2025-05-14 18:56:19 -0300

    vagas

--- src/crud_vagas.py
+++ src/crud_vagas.py
@@ -1,1 +1,16 @@
 # Pedro Marrocos - CRUD de Vagas (empresas criam/editam/excluem/listam vagas)
+import json
+import os
+
+def adcionar_vagas():
+    titulo = str(input("Título da vaga: "))
+    empresa = str(input("Empresa: "))
+    local = str(input("Local: "))
+    vaga = {"titulo": titulo, "empresa": empresa, "local": local}
+    vagas = carregar_vagas()
+    vagas.append (vaga)
+    salvar_vagas = (vagas)
+    print(""✅ Vaga adicionada com sucesso!"")
+    
+def excluir_vaga():
+    
