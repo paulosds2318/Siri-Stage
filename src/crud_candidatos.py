f84a405b29fc5224ba4e950b6b43c679756f3da4
Luiz Henrique <luiz.contato.1000@gmail.com>
2025-05-15 14:33:48 -0300

    adicionando novas feature
    (carregar usuário) / (adicionar usuário) / (listar usuário)

--- src/crud_candidatos.py
+++ src/crud_candidatos.py
@@ -1,41 +1,58 @@
-# Luiz Henrique - CRUD de Candidatos (perfil do aluno: criar/editar/excluir/ver)
 import json
-lista_usuarios = []
+import os
+from random import randint
 
-def menu_inical():
+caminho_arquivo = "data/candidatos.json"
+
+def salvar_json(lista_usuarios, caminho_arquivo):
+    try:
+        with open(caminho_arquivo, 'w', encoding='utf-8') as f:
+            json.dump(lista_usuarios, f, indent=4, ensure_ascii=False)
+        print(f"Dados salvos com sucesso em {caminho_arquivo}!")
+    except Exception as e:
+        print(f"Ocorreu um erro ao salvar em JSON: {e}")
+
+
+def menu_inicial():
     print("1 - Criar Usuário")
     print("2 - Listar Usuário")
     print("3 - Editar Usuário")
     print("4 - Deletar Usuário")
     print("0 - Encerrar o programa")
 
-def salvar_usuarios_json(nome_arquivo="data/candidatos.json"):
-    try:
-        with open(nome_arquivo, 'r+') as arquivo_json:
-            try:
-                dados_existentes = json.load(arquivo_json)
-            except json.JSONDecodeError:
-                dados_existentes = []
 
-            dados_existentes.extend(lista_usuarios)
+def carregar_usuario():
+    if os.path.exists(caminho_arquivo):
+        try:
+            with open(caminho_arquivo, "r", encoding="utf-8") as f:
+                return json.load(f)
+        except json.JSONDecodeError:
+            return []
+    else:
+        return []
 
-            arquivo_json.seek(0)
-            json.dump(dados_existentes, arquivo_json, indent=4)
-            arquivo_json.truncate()
-
-        print(f"Dados dos candidatos salvos com sucesso em '{nome_arquivo}'")
-    except Exception as e:
-        print(f"Ocorreu um erro ao salvar o arquivo JSON: {e}")
 
 def adicionar_usuario():
-    #cria um usuário{} e adiciona a lista de usuário[]
-    usuario = {}
-    usuario["Nome"] = str(input("Nome do usuário: "))
-    usuario["Idade"] = int(input("Digite sua idade: "))
-    usuario["Sexo"] = str(input("Qual seu sexo? "))
-    lista_usuarios.append(usuario.copy())
+    lista_de_usuarios = carregar_usuario()
+    novo_usuario = {}
+    novo_usuario["Nome"] = str(input("Nome do usuário: "))
+    novo_usuario["Idade"] = int(input("Digite sua idade: "))
+    novo_usuario["Sexo"] = str(input("Qual seu sexo? "))
+    novo_usuario["ID"] = randint(0, 100000)
+    lista_de_usuarios.append(novo_usuario.copy())
+    salvar_json(lista_de_usuarios, caminho_arquivo)
+    print("-" * 50)
+    print(f'Usuário Cadastrado Com Sucesso! ID: {novo_usuario["ID"]}')
+    print("-" * 50)
+
+
+def listar_usuario():
+    lista_de_usuarios = carregar_usuario()
+    if not lista_de_usuarios:
+        print("Não há usuários cadastrados!")
+    print("Lista de usuários cadastrados:")
     print("-"*50)
-    print(f'Usuário Cadastrado Com Sucesso!')
-    print("-"*50)
-    salvar_usuarios_json()
-x
+    for pessoa in lista_de_usuarios:
+        print(f"Nome: {pessoa['Nome']} / Idade: {pessoa['Idade']} / Sexo: {pessoa['Sexo']} / ID: {pessoa['ID']} ")
+        print("-"*50)
+
